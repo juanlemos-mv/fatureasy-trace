10fc5a5772da6bb202a88f79706531d6f8af7e70
Juan Guilherme Silva Lemos <juan.lemos@mv.com.br>
2026-05-27 11:07:17 -0300

    chore(project): scaffold fatureasy trace modules

---
+++ trace-app/src/main/java/com/mv/trace/app/TraceAppApplication.java
@@ -0,0 +1,12 @@
+package com.mv.trace.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceAppApplication.class, args);
+    }
+}
